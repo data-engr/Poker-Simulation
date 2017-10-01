93c42cd0ad89f389870cc672736222f197074f38
data-engr <j.n.marey@gmail.com>
2017-09-30 19:02:06 -0500

    Added functionality to specify hand, flop, turn, and river

--- sim.R
+++ sim.R
@@ -14,44 +14,87 @@ suit<-c(spades,clubs,diamonds,hearts)
 
 deck<-paste(value,suit,sep = "")
 
-hand<-function(players=2){
+hand<-function(players=2, player1,flop,turn,river){
+
   ############
   # pre flop #
   ############
   player<-matrix(NA,nrow=players,ncol=2)
-  for(i in 1:2){
-    for(j in 1:players){
-      player[j,i]<-sample(deck,size = 1, replace = FALSE)
-      deck<-deck[-which(deck%in%player)]
+  if(missing(player1)){
+    for(i in 1:2){
+      for(j in 1:players){
+        player[j,i]<-sample(deck,size = 1, replace = FALSE)
+        deck<-deck[-which(deck%in%player)]
+      }
     }
   }
-  return(player)
+  else{
+    deck<-deck[-which(deck%in%player1)]
+    player<-matrix(NA,nrow=players,ncol=2)
+    player[1,]<-player1
+    for(i in 1:2){
+      for(j in 2:players){
+        player[j,i]<-sample(deck,size = 1, replace = FALSE)
+        deck<-deck[-which(deck%in%player)]
+      }
+    }
+  }
+  if(missing(flop)){
+    ########
+    # flop #
+    ########
+    burn<-sample(deck, size=3, replace = FALSE)
+    deck<-deck[-which(deck%in%burn)]
+    
+    board<-sample(deck,size=3,replace=FALSE)
+    deck<-deck[-which(deck%in%board)]
+  }
+  else{
+    board<-flop
+    deck<-deck[-which(deck%in%flop)]
+    burn<-sample(deck,size=3,replace=FALSE)
+    deck<-deck[-which(deck%in%burn)]
+  }
+  
+  if(missing(turn)){
+    ########
+    # turn #
+    ########
+    burn.turn<-sample(deck,size = 1,replace = FALSE)
+    deck<-deck[-which(deck%in%burn.turn)]
+    
+    board[4]<-sample(deck,size = 1,replace = FALSE)
+    deck<-deck[-which(deck%in%board)]
+  }
+  else{
+    board[4]<-turn
+    deck<-deck[-which(deck%in%turn)]
+    burn.turn<-sample(deck,size=1,replace=FALSE)
+    deck<-deck[-which(deck%in%burn.turn)]
+  }
+  
+  if(missing(river)){
+    #########
+    # river #
+    #########
+    burn.river<-sample(deck,size = 1,replace=FALSE)
+    deck<-deck[-which(deck%in%burn.river)]
+    
+    board[5]<-sample(deck,size = 1,replace = FALSE)
+    deck<-deck[-which(deck%in%board)]
+  }
+  else{
+    board[5]<-river
+    deck<-deck[-which(deck%in%river)]
+    burn.river<-sample(deck,size=1,replace=FALSE)
+    deck<-deck[-which(deck%in%burn.river)]
+  }
+  
+  x<-list(player,board)
+  return(x)
 }
 
 
-########
-# flop #
-########
-burn<-sample(deck, size=3, replace = FALSE)
-deck<-deck[-which(deck%in%burn)]
 
-board<-sample(deck,size=3,replace=FALSE)
-deck<-deck[-which(deck%in%board)]
 
-########
-# turn #
-########
-burn.turn<-sample(deck,size = 1,replace = FALSE)
-deck<-deck[-which(deck%in%burn.turn)]
 
-board[4]<-sample(deck,size = 1,replace = FALSE)
-deck<-deck[-which(deck%in%board)]
-
-#########
-# river #
-#########
-burn.river<-sample(deck,size = 1,replace=FALSE)
-deck<-deck[-which(deck%in%burn.river)]
-
-board[5]<-sample(deck,size = 1,replace = FALSE)
-deck<-deck[-which(deck%in%board)]
